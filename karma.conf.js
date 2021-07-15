e6aef8b8a77eb086c1c98b91d97a5bbe22c89564
SOFT2020 <SOFT2020@b05723.com>
2021-07-15 23:54:41 +0530

    Compile Error Fixes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/datatable-demo'),
+      dir: require('path').join(__dirname, './coverage/LH-UI'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'datatable-demo'`, () => {
+  it(`should have as title 'LH-UI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('datatable-demo');
+    expect(app.title).toEqual('dLH-UI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('datatable-demo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('LH-UI app is running!');
   });
 });
